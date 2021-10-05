088ea186f242403e25e34bd6d7179c3be2b2cf80
tarekdinaji <mtarekhossain@gmail.com>
2021-10-05 15:36:49 +0600

    Regular commit October 05, 2021

--- ruby_p/5_code_blocks/7_merge_method.rb
+++ ruby_p/5_code_blocks/7_merge_method.rb
@@ -3,8 +3,10 @@
 # used for hashes only
 # it merges two hashes together
 # we can provide codeblocks to change how it works
-# it merges two hash together, if there is any duplicate keys, the data of the second hash (new one) gets added
+# it merges two hash together, if there is any duplicate keys (key conflict)
+# .. the data of the second hash (new one) gets added
 # we can use codeblocks to force it to keep the old data. 
+# the codeblocks only gets executed when there is a key conflict
 
 # Example of a simple merge method action
   h1 = {:a => 2, :b => 4, :c => 6}
@@ -17,7 +19,8 @@
   h1 = {:a => 2, :b => 4, :c => 6}
   h2 = {:a => 3, :b => 4, :d => 8}
 
-  h1.merge(h2) {|key, old, new| old} # this force it to take the new values  
+  h1.merge(h2) {|key, old, new| old} # this force it to take the old values  
+  # in the syntax above, notice the sequence of k, o and n block variables. 
   # returns: {:a => 2, :b => 4, :c => 6, :d => 8}
 
 
--- ruby_p/6_custom_methods/3_arguments.rb
+++ ruby_p/6_custom_methods/3_arguments.rb
@@ -1,2 +1,16 @@
 #! /usr/bin/env ruby
 
+# Arguments allowes methods to receive values at runtime (when the method is called)
+# It is defiled comma-seperated within the bracket. (after the method name)
+# Arguments are also called "Args"
+# The argument's order of appearance must match the method defined
+# A method can take or not take an argument, it depends on how the method is designed
+
+#Example
+=begin 
+  def volume(x,y,z) #the args are provided inside the bracket
+  x*y*z
+  end
+  volume (2,3,5) #notice it includes three aruments as the method defined
+=end
+
