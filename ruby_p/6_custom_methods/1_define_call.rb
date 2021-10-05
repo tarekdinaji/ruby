8dd030fb581f211021ae6d5038518a2f993e85ab
tarekdinaji <mtarekhossain@gmail.com>
2021-10-05 15:37:26 +0600

    Regular commit October 05, 2021

--- ruby_p/6_custom_methods/1_define_call.rb
+++ ruby_p/6_custom_methods/1_define_call.rb
@@ -1,2 +1,28 @@
-#! /usr/bin/env ruby
+#!/usr/bin/env ruby
 
+# in other programming language a method may be called as function
+# Don't repeat yourself (DRY), is implemented using method
+# A method can include leters,digits and underscores
+# A method's last character could be ?!=
+# if the last character is ? then it will return boolean
+# if the last character is ! then it should be a more roboust/destructive version
+# The method identifire is "def" and "end"
+
+#Example:
+=begin  
+  def some_name
+    #..........
+  end
+=end  
+
+#Example 2: 
+def blanket_patterns(colors, lines)
+  lines.times do |i|
+    first = colors[0]
+    rest = colors [1..-1]
+    colors = rest + first
+    puts colors
+  end
+end
+
+blanket_patterns("++*~~*++*", 20)
--- ruby_p/6_custom_methods/2_variable_scope.rb
+++ ruby_p/6_custom_methods/2_variable_scope.rb
@@ -1,2 +1,16 @@
 #! /usr/bin/env ruby
 
+# Global | $variable
+# Class  | @@variable
+# Instance | @variable
+# Local  | variable
+# Block  | variable
+
+# variables that are defined inside a method are local variables
+# method's local variables can't be accessd outside of that method
+# If we want to use outside variable inside a method 
+# .. we need to use different variable type that is an instance type variable
+# We can also pass in outside variables as arguments
+
+#?? Can variables set outside be accessed from inside the method?
+#??  
