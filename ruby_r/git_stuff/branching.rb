c152b0aa7f32a607db91db4caf4114ad71dc618f
tarekdinaji <mtarekhossain@gmail.com>
2021-10-06 12:04:12 +0600

    acepting Faruk's remarks

---
+++ ruby_r/git_stuff/branching.rb
@@ -0,0 +1,24 @@
+# git checkout -b testbranch
+# status
+#.. git branch
+
+# checking remote branchlist status
+#.. git branch -r
+# checking all branchlist status
+#.. git branch -a
+
+#creating a new branch on remote
+# .. git push -u origin testbranch 
+
+# moving to staging
+#.. git add . 
+
+# checking status
+#.. git status
+
+# commiting git 
+# git commit -m "message"
+
+# pushing to remote
+#.. git push
+
