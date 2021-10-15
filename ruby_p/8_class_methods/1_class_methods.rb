b7fe937cc9117472019800010b65d8f1bd73e0b9
tarekdinaji <mtarekhossain@gmail.com>
2021-10-15 22:21:29 +0600

    regular push

--- ruby_p/8_class_methods/1_class_methods.rb
+++ ruby_p/8_class_methods/1_class_methods.rb
@@ -5,6 +5,8 @@
 # Animal.new
 # Invoice.find(4954)
 # Cars.all_brands
+# We can differentiate between Class and Instance methods by looking
+#.. A class method is defined using self.method_name 
 
 # There is a difference in how class or instance methods are caled
 #.. Class method: Array.new, Instance method: Array#size
@@ -38,6 +40,7 @@ puts sprintf('$%.2f', toyota.price)
 
 class Product
   attr_accessor :name, :price
-
   def self.soldout
     Database
+  end
+end
